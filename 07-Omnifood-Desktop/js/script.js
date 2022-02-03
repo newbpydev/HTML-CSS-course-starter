309ff16fb9b60364cb6fe85905982e5aed9b7053
newbpydev <newbpydev@gmail.com>
2022-02-03 18:21:59 -0400

    Add smooth scrolling using js

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -23,8 +23,47 @@ btnNavEl.addEventListener("click", function () {
 });
 // .nav-open
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
+//* /////////////////////////////////////////////////////////
+//* Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    // console.log(href)
+
+    //* Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //* Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+
+      //* Scroll back to top
+      if (link.classList.contains("main-nav-link")) {
+        // headerEl.classList.remove("nav-open");
+        headerEl.classList.toggle("nav-open");
+      }
+
+      // btnNavEl.addEventListener("click", function () {
+      //   headerEl.classList.toggle("nav-open");
+      // });
+    }
+  });
+});
+
+//* /////////////////////////////////////////////////////////
+//* Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
